60d3eb4f1c80589cea3149141fdc82d5f0ae043d
Mitsuharu UEMOTO <uemoto@eedept.kobe-u.ac.jp>
2022-01-30 15:29:18 +0900

    fix: namelist parser

---
+++ namelist2.js
@@ -0,0 +1,203 @@
+// FORTRAN Namelist形式データ読み込み
+
+class Namelist {
+    constructor(rule) {
+        this.rule = this._deepCopy(rule);
+        this.error = [];
+        this.warning = [];
+        this.ignore = [];
+        this.reset();
+    }
+
+    reset() {
+        this.data = {};
+        for(var group in this.rule) {
+            this.data[group] = {};
+            for(var title in this.rule[group])
+                this.data[group][title] = this._genDefaultArray(group, title);
+        }
+    }
+
+    parse(code) {
+        var err = [], warn = [];
+        var group = "";
+        var line = code.split(/\r?\n/);
+        for(var i=0; i<line.length; i++) {
+            var str = line[i];
+
+            // スペースとコメントを削除
+            str = str.replace(/^\s*/, "");
+            str = str.replace(/\s*(!.*)?$/, "");
+            
+            // 空行は読み飛ばす
+            if (!str) continue;
+            
+            // グループ開始部分(&xxxx)を検出
+            var tmp = str.match(/^&(\w+)$/);
+            if (tmp) {
+                // 前回のグループがまだ閉じていない場合
+                if (group)
+                    err.push({lineno:i+1, msg:"previous group is not closed!"});
+                group = tmp[1].toLowerCase();
+                // 未定義のグループ名の場合
+                if (this._isUndefinedGroup(group))
+                    warn.push({lineno:i+1, msg:"group name is invalid!"});
+                continue;
+            }
+
+            // グループ終了部分を検出
+            var tmp = str.match(/^\/$/);
+            if (tmp) {
+                // グループがまだ閉じていない場合
+                if (! group)
+                    err.push({lineno:i+1, msg:"group is not opened!"});
+                group = "";
+                continue;
+            }
+
+            if (this.ignore.includes(group)) continue;
+
+            // 代入文を検出
+            var tmp = str.match(/^(\w+)\s*(\(.*\))?\s*=\s*(.+)$/);
+            if (tmp) {
+                var title = tmp[1].toLowerCase();
+
+                if (! ((group in this.rule) && (title in this.rule[group]))) {
+                    warn.push({lineno:i+1, msg:"undefined variable name or group!"});
+                    continue;
+                }
+
+                var indices = tmp[2];
+                var values = tmp[3];
+                var err_tmp = this._assign(group, title, indices, values);
+                if (err_tmp)
+                    err.push({lineno:i+1, msg:err_tmp});
+
+                continue;
+            }
+            
+            err.push({lineno:i+1, msg:"invalid syntax!"});
+        }
+
+        this.error = err;
+        this.warning = warn;
+        
+
+    }
+
+    _deepCopy(x) {
+        return JSON.parse(JSON.stringify(x));
+    }
+
+    _isUndefinedGroup(group) {
+        return ! ((group in this.rule) || (this.ignore.includes(group)));
+    }
+
+    _getArrayDimension(group, title) {
+        var tmp = this.rule[group][title];
+        // if (tmp.kmax != undefined) return 3;
+        if (tmp.jmax != undefined) return 2;
+        if (tmp.imax != undefined) return 1;
+        return 0;
+    }
+
+    _parseFortranValue(str, type) {
+        if (type.match(/^integer/i)) {
+            if (str.match(/^\s*[+-]?\s*\d+\s*$/)) {
+                var tmp = parseInt(str);
+                if (! isNaN(tmp)) return tmp;
+            }
+        } else if (type.match(/^real/i)) {
+            if (str.match(/^\s*[+-]?\s*\d*\.?\d+([ed][+-]?\d+)?\s*$/i)) {                
+                var tmp = parseFloat(str.replace(/d/i, "e"));
+                if (! isNaN(tmp)) return tmp;
+            }
+        } else if (type.match(/^character/i)) {
+            var tmp = str.match(/'(.*)'/);
+            if (tmp) return tmp[1];
+            var tmp = str.match(/"(.*)"/);
+            if (tmp) return tmp[1];
+            return str;
+        }
+        return undefined;
+    }
+
+
+    _genDefaultArray(group, title) {
+        var target = this.rule[group][title];
+        // 初期値が指定されている場合は単純コピーを行う
+        if (target.default_val instanceof Object)
+            return this._deepCopy(target.default_val);
+        // 初期値がスカラの場合は配列を生成する
+        switch (this._getArrayDimension(group, title)) {
+        case 0:
+            return target.default_val;
+        case 1:
+            var imax = target.imax;
+            var imin = (target.imin == undefined) ? 1 : target.imin;
+            var tmp = {};
+            for(var i=imin; i<=imax; i++)
+                tmp[i] = target.default_val;
+            return tmp;
+        case 2:
+            var imax = target.imax;
+            var imin = (target.imin == undefined) ? 1 : target.imin;
+            var jmax = target.jmax;
+            var jmin = (target.jmin == undefined) ? 1 : target.jmin;
+            var tmp = {}
+            for (var i=imin; i<=imax; i++) {
+                tmp[i] = {};
+                for (var j=jmin; j<jmax; j++) {
+                    tmp[i][j] = target.default_val;
+                }
+            }
+            return tmp;
+        }
+    }
+
+    _assign(group, title, indices, values) {
+        var target = this.rule[group][title];
+        switch (this._getArrayDimension(group, title)) {
+        case 0: // スカラ変数の代入
+            var x = this._parseFortranValue(values, target.type);
+            if (x == undefined) return "invalid format of " + target.type;
+            this.data[group][title] = x;
+            break;
+        case 1: // 一次元配列の代入
+            if (indices == undefined) return "unsupported assginment style";
+            // 要素指定型の代入
+            var imax = target.imax;
+            var imin = (target.imin == undefined) ? 1 : target.imin;
+            var tmp = indices.match(/\(\s*([+-]?\s*\d+)\s*\)/);
+            if (tmp) {
+                i = parseInt(tmp[1]);
+                if (! (imin <= i && i <= imax)) return "out of index range";
+                var x = this._parseFortranValue(values, target.type);
+                if (x == undefined) return "invalid format of " + target.type;
+                this.data[group][title][i] = x;
+                break;
+            }
+            // スライス型代入
+            var tmp = indices.match(/\(\s*([+-]?\s*\d*)\s*:\s*([+-]?\s*\d*)\s*\)/);
+            if (tmp) {
+                var iimin = (tmp[1] == undefined) ? imin : parseInt(tmp[1]);
+                var iimax = (tmp[2] == undefined) ? imax : parseInt(tmp[2]);
+                if (! (imin <= iimin && iimin <= iimax && iimax <= imax))
+                    return "out of index range";
+                var tmp = values.split(/\s*,\s*/);
+                if (tmp.length != (iimax - iimin + 1))
+                    return "assignment array size mismatch";
+                for (var i=iimin; i<=iimax; i++) {
+                    var x = this._parseFortranValue(tmp[i-imin], target.type);
+                    if (x == undefined) return "invalid format of " + target.type;
+                    this.data[group][title][i] = x;
+                }
+                break;
+            }
+            return "unsupported assginment style";
+        }
+        return;
+    }
+
+
+}
--- script.js
+++ script.js
@@ -1,41 +1,223 @@
-target = document.getElementById("target");
-console.log("Start");
+// target = document.getElementById("target");
+// console.log("Start");
 
 
-waveplot = new Waveplot(target);
+// waveplot = new Waveplot(target);
 
-waveplot.plot();
+// waveplot.plot();
 
 panelLeft = document.getElementById("panelLeft");
+panelRight = document.getElementById("panelRight");
+panelViewer = document.getElementById("panelViewer");
+panelFooter = document.getElementById("panelFooter");
+panelEditor = document.getElementById("panelEditor")
+btnPlot = document.getElementById("btnPlot");
 
-var editor = new Editor(panelLeft);
-// スクロールイベントの検出
+// エディタ画面を初期化
+var editor = new Editor(panelEditor);
 editor.textarea.onscroll = function(){editor.scroll()};
 
 
 // Resizing Problem
+samplecode = `!########################################################################################!
+! Excercise 06: Electron dynamics in crystalline silicon under a pulsed electric field   !
+!----------------------------------------------------------------------------------------!
+! * The detail of this excercise is expained in our manual(see chapter: 'Exercises').    !
+!   The manual can be obtained from: https://salmon-tddft.jp/documents.html              !
+! * Input format consists of group of keywords like:                                     !
+!     &group                                                                             !
+!       input keyword = xxx                                                              !
+!     /                                                                                  !
+!   (see chapter: 'List of input keywords' in the manual)                                !
+!----------------------------------------------------------------------------------------!
+! * Conversion from unit_system = 'a.u.' to 'A_eV_fs':                                   !
+!   Length: 1 [a.u.] = 0.52917721067    [Angstrom]                                       !
+!   Energy: 1 [a.u.] = 27.21138505      [eV]                                             !
+!   Time  : 1 [a.u.] = 0.02418884326505 [fs]                                             !
+!----------------------------------------------------------------------------------------!
+! * Copy the ground state data directory('data_for_restart') (or make symbolic link)     !
+!   calculated in 'samples/exercise_04_bulkSi_gs/' and rename the directory to 'restart/'!
+!   in the current directory.                                                            !
+!########################################################################################!
+
+&calculation
+  !type of theory
+  theory = 'tddft_pulse'
+/
+
+&control
+  !common name of output files
+  sysname = 'Si'
+/
+
+&units
+  !units used in input and output files
+  unit_system = 'a.u.'
+/
+
+&system
+  !periodic boundary condition
+  yn_periodic = 'y'
+  
+  !grid box size(x,y,z)
+  al(1:3) = 10.26d0, 10.26d0, 10.26d0
+  
+  !number of elements, atoms, electrons and states(bands)
+  nelem  = 1
+  natom  = 8
+  nelec  = 32
+  nstate = 32
+/
+
+&pseudo
+  !name of input pseudo potential file
+  file_pseudo(1) = './Si_rps.dat'
+  
+  !atomic number of element
+  izatom(1) = 14
+  
+  !angular momentum of pseudopotential that will be treated as local
+  lloc_ps(1) = 2
+  !--- Caution -------------------------------------------!
+  ! Index must correspond to those in &atomic_red_coor.   !
+  !-------------------------------------------------------!
+/
+
+&functional
+  !functional('PZ' is Perdew-Zunger LDA: Phys. Rev. B 23, 5048 (1981).)
+  xc = 'PZ'
+/
+
+&rgrid
+  !number of spatial grids(x,y,z)
+  num_rgrid(1:3) = 12, 12, 12
+/
+
+&kgrid
+  !number of k-points(x,y,z)
+  num_kgrid(1:3) = 4, 4, 4
+/
+
+&tgrid
+  !time step size and number of time grids(steps)
+  dt = 0.08d0
+  nt = 6000
+/
+
+&emfield
+  !envelope shape of the incident pulse('Acos2': cos^2 type envelope for vector potential)
+  ae_shape1 = 'Acos2'
+  
+  !peak intensity(W/cm^2) of the incident pulse
+  I_wcm2_1 = 5.0d11
+  
+  !duration of the incident pulse
+  tw1 = 441.195136248d0
+  
+  !mean photon energy(average frequency multiplied by the Planck constant) of the incident pulse
+  omega1 = 0.05696145187d0
+  
+  !polarization unit vector(real part) for the incident pulse(x,y,z)
+  epdir_re1(1:3) = 0.0d0, 0.0d0, 1.0d0
+  !--- Caution ---------------------------------------------------------!
+  ! Defenition of the incident pulse is wrriten in:                     !
+  ! https://www.sciencedirect.com/science/article/pii/S0010465518303412 !
+  !---------------------------------------------------------------------!
+/
+
+&atomic_red_coor
+  !cartesian atomic reduced coodinates
+  'Si'	.0	.0	.0	1
+  'Si'	.25	.25	.25	1
+  'Si'	.5	.0	.5	1
+  'Si'	.0	.5	.5	1
+  'Si'	.5	.5	.0	1
+  'Si'	.75	.25	.75	1
+  'Si'	.25	.75	.75	1
+  'Si'	.75	.75	.25	1
+  !--- Format ---------------------------------------------------!
+  ! 'symbol' x y z index(correspond to that of pseudo potential) !
+  !--------------------------------------------------------------!
+/
+`;
 
 function setup() {
-    panelLeft.style.top = "0px";
-    panelRight.style.top = "0px";
+    editor.textarea.value =samplecode;
+}
 
+
+rule = {
+    "calculation": {
+        "theory": {type:'character(64)', default_val:"", },
+    },
+    "control": {
+        "sysname": {type:'character(64)', default_val:"", },
+    },
+    "units": {
+        "unit_system": {type:'character(64)', default_val:"", },
+    },
+    "system": {
+        "yn_periodic": {type:'character(64)', default_val:"", },
+        "al": {type:'real(8)', default_val:0.0, imin:1, imax:3, },
+        "nelem": {type:'integer', default_val:0, },
+        "natom": {type:'integer', default_val:0, },
+        "nelec": {type:'integer', default_val:0, },
+        "nstate": {type:'integer', default_val:0, },
+    },
+    "pseudo": {
+        "file_pseudo": {type:'character(64)', default_val:"", imin:1, imax:99 },
+        "izatom": {type:'integer', default_val:0, imin:1, imax:99},
+        "lloc_ps": {type:'integer', default_val:0, imin:1, imax:99},
+    },
+    "functional": {
+        "xc": {type:'character(64)', default_val:"", },
+    },
+    "rgrid": {
+        "num_rgrid": {type:'integer', default_val:0, imin:1, imax:3},
+    },
+    "kgrid": {
+        "num_kgrid": {type:'integer', default_val:0, imin:1, imax:3},
+    },
+    "tgrid": {
+        "dt": {type:'real(8)', default_val:0.0, },
+        "nt": {type:'integer', default_val:0, },
+    },
+    "emfield": {
+        "ae_shape1": {type:'character(64)', default_val:"", },
+        "i_wcm2_1": {type:'real(8)', default_val:0.0, },
+        "tw1": {type:'real(8)', default_val:0.0, },
+        "omega1": {type:'real(8)', default_val:0.0, },
+        "epdir_re1": {type:'real(8)', default_val:0.0, imin:1, imax:3, },
+    },
+}
+
+
+function plot() {
+    console.log("START");
+    var nml = new Namelist(rule);
+    nml.ignore = ["atomic_red_coor"];
+    nml.parse(editor.textarea.value);
+    console.log(nml.data);
+    console.log(nml.warning);
+    console.log(nml.error);
 }
 
 function resize() {
-    panelLeft.style.width = (window.innerWidth * 0.5) + "px";
-    panelLeft.style.height = (window.innerHeight) + "px";
-    panelRight.style.left = (window.innerWidth * 0.5) + "px";
-    panelRight.style.width = (window.innerWidth * 0.5) + "px";
-    panelRight.style.height = (window.innerHeight) + "px";
+    // Setup Panel size
+    panelLeft.style.height = (window.innerHeight - panelLeft.offsetTop - panelFooter.offsetHeight) + "px";
+    panelRight.style.height = (window.innerHeight - panelRight.offsetTop - panelFooter.offsetHeight) + "px";
+    panelViewer.style.height = (window.innerHeight - panelViewer.offsetTop - panelFooter.offsetHeight) + "px";
+    panelEditor.style.height = (window.innerHeight - panelEditor.offsetTop - panelFooter.offsetHeight - 8) + "px";
     editor.resize();
 }
 
-
-var resize_timer = null;
+var resize_timer = undefined;
 window.onresize = function() {
-    if (resize_timer !== null) clearTimeout(resize_timer);
+    if (resize_timer != undefined) clearTimeout(resize_timer);
     resize_timer = setTimeout(resize, 50);
 };
+
+btnPlot.onclick = plot;
 
 setup();
 
